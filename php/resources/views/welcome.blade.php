1b4e973b82fc1f4422541b4d2ca9f3ccfa488573
lucaszebre <lucaszebre1@gmail.com>
2026-03-14 14:38:52 +0100

    Add php application to parent repo

---
+++ php/resources/views/welcome.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    @php
+    $serverLabel = request()->server('APP_SERVER_LABEL', 'Serveur inconnu');
+    @endphp
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+            @endauth
+        </div>
+        @endif
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <h1 class="text-4xl font-bold text-gray-800 dark:text-white mb-4">{{ $serverLabel }}</h1>
+            <a href="http://localhost:8080/" target="_self" class="cursor-pointer underline mr-4">
+                Nginx 1
+            </a>
+            <a href="http://localhost:8081/" target="_self" class="cursor-pointer underline">
+                Nginx 2
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
